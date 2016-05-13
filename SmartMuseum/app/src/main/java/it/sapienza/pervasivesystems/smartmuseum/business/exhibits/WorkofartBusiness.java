d9fc66222d6050461ec219d86bd7781145e7f7d4
GuamaralVasili <guamaral2005@yahoo.com>
2016-05-13 02:18:14 +0200

    List of exhibits

--- SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/business/exhibits/WorkofartBusiness.java
+++ SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/business/exhibits/WorkofartBusiness.java
@@ -34,7 +34,7 @@ public class WorkofartBusiness {
         w1.setTitle("The Fountains, 1787/88");
         w1.setShortDescription("The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells..");
         w1.setLongDescription("Neoclassical ruin painting had reached its zenith of popularity when Hubert Robert, its foremost French practitioner, was commissioned in 1787 to paint a suite of four canvases for a wealthy financier’s château at Méréville, France. Robert had gleaned his artistic vocabulary from more than a decade’s study in Rome (1754–65). Like the other three large paintings from the group, all in the Art Institute, The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells, decaying colonnades, and Roman statuary that create a fantasy of expansive space. The four paintings are inhabited by tiny figures in the foreground; they serve only to set the scale and animate the scene, for the ruins themselves are the true subject of the pictures. The Fountains and its companion pieces were set into the paneled walls of one of the château’s salons, creating an alternate space that played off of the elegant, Neoclassical decor of the room. In his use of ruins, Robert embodied the Romantic concept of the relationship of man and his works to nature, as expressed by the French philosopher and encyclopedist Denis Diderot: \"Everything vanishes, everything dies, only time endures.\"");
-        w1.setImage("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/images/image0.jpg")));
+        w1.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/2048_8066/images/image0.jpg");
         w1.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/audio/audio0.mp3")));
         list.add(w1);
         WorkofartModel w2 = new WorkofartModel();
@@ -42,9 +42,61 @@ public class WorkofartBusiness {
         w2.setTitle("Washerwomen in the Ruins of the Colosseum");
         w2.setShortDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments.");
         w2.setLongDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments. During his eleven-year stay in Italy, Robert assembled a vast “catalogue” of drawings of such sites, which he would later include in the historically evocative fantasy scenes that earned him both patronage and acclaim. In this painting’s combination of real and imagined structures, the recognizable pierced walls of Rome’s Colosseum loom over the coffered arches of another decaying structure, while washerwomen gather around a fire.");
-        w2.setImage("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/images/image0.jpg")));
+        w2.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
         w2.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/audio/audio0.mp3")));
         list.add(w2);
+
+        WorkofartModel w3 = new WorkofartModel();
+        w3.setIdWork(3);
+        w3.setTitle("The Fountains, 1787/88");
+        w3.setShortDescription("The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells..");
+        w3.setLongDescription("Neoclassical ruin painting had reached its zenith of popularity when Hubert Robert, its foremost French practitioner, was commissioned in 1787 to paint a suite of four canvases for a wealthy financier’s château at Méréville, France. Robert had gleaned his artistic vocabulary from more than a decade’s study in Rome (1754–65). Like the other three large paintings from the group, all in the Art Institute, The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells, decaying colonnades, and Roman statuary that create a fantasy of expansive space. The four paintings are inhabited by tiny figures in the foreground; they serve only to set the scale and animate the scene, for the ruins themselves are the true subject of the pictures. The Fountains and its companion pieces were set into the paneled walls of one of the château’s salons, creating an alternate space that played off of the elegant, Neoclassical decor of the room. In his use of ruins, Robert embodied the Romantic concept of the relationship of man and his works to nature, as expressed by the French philosopher and encyclopedist Denis Diderot: \"Everything vanishes, everything dies, only time endures.\"");
+        w3.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/2048_8066/images/image0.jpg");
+        w3.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/audio/audio0.mp3")));
+        list.add(w3);
+        WorkofartModel w4 = new WorkofartModel();
+        w4.setIdWork(4);
+        w4.setTitle("Washerwomen in the Ruins of the Colosseum");
+        w4.setShortDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments.");
+        w4.setLongDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments. During his eleven-year stay in Italy, Robert assembled a vast “catalogue” of drawings of such sites, which he would later include in the historically evocative fantasy scenes that earned him both patronage and acclaim. In this painting’s combination of real and imagined structures, the recognizable pierced walls of Rome’s Colosseum loom over the coffered arches of another decaying structure, while washerwomen gather around a fire.");
+        w4.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
+        w4.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/audio/audio0.mp3")));
+        list.add(w4);
+
+        WorkofartModel w5 = new WorkofartModel();
+        w5.setIdWork(5);
+        w5.setTitle("The Fountains, 1787/88");
+        w5.setShortDescription("The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells..");
+        w5.setLongDescription("Neoclassical ruin painting had reached its zenith of popularity when Hubert Robert, its foremost French practitioner, was commissioned in 1787 to paint a suite of four canvases for a wealthy financier’s château at Méréville, France. Robert had gleaned his artistic vocabulary from more than a decade’s study in Rome (1754–65). Like the other three large paintings from the group, all in the Art Institute, The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells, decaying colonnades, and Roman statuary that create a fantasy of expansive space. The four paintings are inhabited by tiny figures in the foreground; they serve only to set the scale and animate the scene, for the ruins themselves are the true subject of the pictures. The Fountains and its companion pieces were set into the paneled walls of one of the château’s salons, creating an alternate space that played off of the elegant, Neoclassical decor of the room. In his use of ruins, Robert embodied the Romantic concept of the relationship of man and his works to nature, as expressed by the French philosopher and encyclopedist Denis Diderot: \"Everything vanishes, everything dies, only time endures.\"");
+        w5.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/2048_8066/images/image0.jpg");
+        w5.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/audio/audio0.mp3")));
+        list.add(w5);
+        WorkofartModel w6 = new WorkofartModel();
+        w6.setIdWork(6);
+        w6.setTitle("Washerwomen in the Ruins of the Colosseum");
+        w6.setShortDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments.");
+        w6.setLongDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments. During his eleven-year stay in Italy, Robert assembled a vast “catalogue” of drawings of such sites, which he would later include in the historically evocative fantasy scenes that earned him both patronage and acclaim. In this painting’s combination of real and imagined structures, the recognizable pierced walls of Rome’s Colosseum loom over the coffered arches of another decaying structure, while washerwomen gather around a fire.");
+        w6.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
+        w6.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/audio/audio0.mp3")));
+        list.add(w6);
+
+        WorkofartModel w7 = new WorkofartModel();
+        w7.setIdWork(7);
+        w7.setTitle("The Fountains, 1787/88");
+        w7.setShortDescription("The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells..");
+        w7.setLongDescription("Neoclassical ruin painting had reached its zenith of popularity when Hubert Robert, its foremost French practitioner, was commissioned in 1787 to paint a suite of four canvases for a wealthy financier’s château at Méréville, France. Robert had gleaned his artistic vocabulary from more than a decade’s study in Rome (1754–65). Like the other three large paintings from the group, all in the Art Institute, The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells, decaying colonnades, and Roman statuary that create a fantasy of expansive space. The four paintings are inhabited by tiny figures in the foreground; they serve only to set the scale and animate the scene, for the ruins themselves are the true subject of the pictures. The Fountains and its companion pieces were set into the paneled walls of one of the château’s salons, creating an alternate space that played off of the elegant, Neoclassical decor of the room. In his use of ruins, Robert embodied the Romantic concept of the relationship of man and his works to nature, as expressed by the French philosopher and encyclopedist Denis Diderot: \"Everything vanishes, everything dies, only time endures.\"");
+        w7.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/2048_8066/images/image0.jpg");
+        w7.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/audio/audio0.mp3")));
+        list.add(w7);
+        WorkofartModel w8 = new WorkofartModel();
+        w8.setIdWork(8);
+        w8.setTitle("Washerwomen in the Ruins of the Colosseum");
+        w8.setShortDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments.");
+        w8.setLongDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments. During his eleven-year stay in Italy, Robert assembled a vast “catalogue” of drawings of such sites, which he would later include in the historically evocative fantasy scenes that earned him both patronage and acclaim. In this painting’s combination of real and imagined structures, the recognizable pierced walls of Rome’s Colosseum loom over the coffered arches of another decaying structure, while washerwomen gather around a fire.");
+        w8.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
+        w8.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/audio/audio0.mp3")));
+        list.add(w8);
+
         return list;
     }
 
@@ -57,7 +109,7 @@ public class WorkofartBusiness {
                 w1.setTitle("The Fountains, 1787/88");
                 w1.setShortDescription("The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells..");
                 w1.setLongDescription("Neoclassical ruin painting had reached its zenith of popularity when Hubert Robert, its foremost French practitioner, was commissioned in 1787 to paint a suite of four canvases for a wealthy financier’s château at Méréville, France. Robert had gleaned his artistic vocabulary from more than a decade’s study in Rome (1754–65). Like the other three large paintings from the group, all in the Art Institute, The Fountains exploits Robert’s stock-in-trade: fictive niches, arches, coffered vaults, majestic stairwells, decaying colonnades, and Roman statuary that create a fantasy of expansive space. The four paintings are inhabited by tiny figures in the foreground; they serve only to set the scale and animate the scene, for the ruins themselves are the true subject of the pictures. The Fountains and its companion pieces were set into the paneled walls of one of the château’s salons, creating an alternate space that played off of the elegant, Neoclassical decor of the room. In his use of ruins, Robert embodied the Romantic concept of the relationship of man and his works to nature, as expressed by the French philosopher and encyclopedist Denis Diderot: \"Everything vanishes, everything dies, only time endures.\"");
-                w1.setImage("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/images/image0.jpg")));
+                w1.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
                 w1.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/1/audio/audio0.mp3")));
                 ret = w1;
                 break;
@@ -67,7 +119,7 @@ public class WorkofartBusiness {
                 w2.setTitle("Washerwomen in the Ruins of the Colosseum");
                 w2.setShortDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments.");
                 w2.setLongDescription("In 1754 Hubert Robert traveled to Italy to further his artistic training and soon found himself captivated by the crumbling ruins of ancient monuments. During his eleven-year stay in Italy, Robert assembled a vast “catalogue” of drawings of such sites, which he would later include in the historically evocative fantasy scenes that earned him both patronage and acclaim. In this painting’s combination of real and imagined structures, the recognizable pierced walls of Rome’s Colosseum loom over the coffered arches of another decaying structure, while washerwomen gather around a fire.");
-                w2.setImage("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/images/image0.jpg")));
+                w2.setImage("https://s3-eu-west-1.amazonaws.com/smartmuseum/exhibits/20512_25367/images/image0.jpg");
                 w2.setAudioURL("https://".concat(AWSConfiguration.awsHostname.concat("/smartmuseum/exhibits/2048_8066/worksofart/2/audio/audio0.mp3")));
                 ret = w2;
                 break;
