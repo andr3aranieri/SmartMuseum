8ca224b0fa98d9691f9d49d7905cf1e23aa62c6d
GuamaralVasili <guamaral2005@yahoo.com>
2016-07-11 00:50:41 +0200

    Add menu

--- SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/view/ListOfUHExhibitsActivity.java
+++ SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/view/ListOfUHExhibitsActivity.java
@@ -4,8 +4,13 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.ListView;
 
@@ -23,6 +28,8 @@ public class ListOfUHExhibitsActivity extends AppCompatActivity {
     ExhibitModelArrayAdapter exhibitAdapter;
     ArrayList<ExhibitModel> dataItems = null;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +43,35 @@ public class ListOfUHExhibitsActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listview);
         listView.setItemsCanFocus(false);
         listView.setAdapter(exhibitAdapter);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            System.out.println("******ACTION logout*********");
+            return true;
+        }
+        if(id == R.id.action_ask) {
+            System.out.println("******ACTION ask*********");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
