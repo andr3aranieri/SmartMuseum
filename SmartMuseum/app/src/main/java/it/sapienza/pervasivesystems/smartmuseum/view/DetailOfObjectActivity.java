3f949da63d829436c3d0100688f0bea062f0bcf8
GuamaralVasili <guamaral2005@yahoo.com>
2016-07-12 03:26:19 +0200

    Add menu

--- SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/view/DetailOfObjectActivity.java
+++ SmartMuseum/app/src/main/java/it/sapienza/pervasivesystems/smartmuseum/view/DetailOfObjectActivity.java
@@ -122,8 +122,8 @@ public class DetailOfObjectActivity extends AppCompatActivity implements View.On
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
             //TODO
-            System.out.println("******ACTION logout*********");
-            return true;
+            Intent intent = new Intent(this, LoginActivity.class);
+            this.startActivity(intent);
         }
         if(id == R.id.action_ask) {
             Intent intent = new Intent(this, MainChatActivity.class);
